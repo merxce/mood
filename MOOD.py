8768ffc28ba6a02f61a99a472712b8914c6b8305
checogarcon <checogarcon@gmail.com>
2026-05-26 18:32:59 -0600

    MOOD.py

--- MOOD.py
+++ MOOD.py
@@ -160,4 +160,4 @@ def recomendar(nombre):
 
 if __name__ == "__main__":
     debug_mode = os.environ.get("FLASK_ENV") == "development"
-    app.run(host="0.0.0.0", port=5000, debug=debug_mode)
+    app.run(host="0.0.0.0", port=8080, debug=debug_mode)
